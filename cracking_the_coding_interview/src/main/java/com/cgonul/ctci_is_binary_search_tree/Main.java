87e7e371c1e04edeb48df40e2bfc30098ba0109b
cgonul <caglar.gonul.001@gmail.com>
2018-02-28 22:18:54 +0300

    master

--- cracking_the_coding_interview/src/main/java/com/cgonul/ctci_is_binary_search_tree/Main.java
+++ cracking_the_coding_interview/src/main/java/com/cgonul/ctci_is_binary_search_tree/Main.java
@@ -3,36 +3,65 @@ package com.cgonul.ctci_is_binary_search_tree;
 public class Main {
 
 	public static void main(String[] args) {
+		//		Node node001 = new Node(1, null, null);
+		//		Node node003 = new Node(3, null, null);
+		//		Node node002 = new Node(2, node001, node003);
+		//
+		//		Node node005 = new Node(5, null, null);
+		//		Node node007 = new Node(7, null, null);
+		//		Node node006 = new Node(6, node005, node007);
+		//
+		//		Node node004 = new Node(4, node002, node006);
+
 		Node node001 = new Node(1, null, null);
-		Node node003 = new Node(3, null, null);
-		Node node002 = new Node(2, node001, node003);
-
 		Node node005 = new Node(5, null, null);
-		Node node007 = new Node(7, null, null);
-		Node node006 = new Node(6, node005, node007);
+		Node node002 = new Node(2, node001, node005);
 
+		Node node003 = new Node(3, null, null);
+		Node node007 = new Node(7, null, null);
+		Node node006 = new Node(6, node003, node007);
 		Node node004 = new Node(4, node002, node006);
+
 		System.out.println(checkBST(node004));
 	}
 
 	private static boolean constraints(int data) {
-		return data >= 0 && data <= 10000;
+		return data < 0 || data > 10000;
+	}
+
+	private static boolean checkBSTHelper(Node root, int rootData) {
+		if(root == null){
+			return true;
+		}
+
+		if(constraints(root.data)){
+			return false;
+		}
+
+		if(root.left != null && root.data < root.left.data){
+			return false;
+		}
+
+		if(root.right != null && root.data > root.right.data){
+			return false;
+		}
+
+		if(root.left != null && root.left.data > rootData){
+			return false;
+		}
+
+		if(root.right != null && root.right.data < rootData){
+			return false;
+		}
+
+		return checkBSTHelper(root.left, rootData) && checkBSTHelper(root.right, rootData);
 	}
 
 	private static boolean checkBST(Node root) {
-		if(root.left != null && root.right != null){
-			return constraints(root.left.data)
-					&& constraints(root.right.data)
-					&& root.data > root.left.data
-					&& root.data < root.right.data
-					&& checkBST(root.left)
-					&& checkBST(root.right);
-		} else if(root.left == null && root.right != null){
-			return false;
-		} else if(root.left != null){
-			return false;
-		} else {
+		if(root == null){
 			return true;
 		}
+		return checkBSTHelper(root, root.data);
 	}
 }
+
